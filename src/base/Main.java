fde3ae74ef900f1956d6c510c732b6534d7d0434
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2020-11-19 18:46:17 -0300

    Add folders

---
+++ src/base/Main.java
@@ -0,0 +1,13 @@
+package base;
+
+public class Main 
+{
+	/**
+	 * Main thread of the program
+	 * @param args arguments passed from console
+	 */
+	public static void main(String[] args) 
+	{		
+
+	}		
+}
---
+++ src/converters/AbstractConverter.java
@@ -0,0 +1,5 @@
+package converters;
+
+public abstract class AbstractConverter {
+
+}
