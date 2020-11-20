62891bfee86a0dfa8de11da788930f10000b1e92
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2020-11-20 19:53:33 -0300

    ClassFinder

--- src/base/Main.java
+++ src/base/Main.java
@@ -9,7 +9,11 @@ public class Main {
 	 * @param args arguments passed from console
 	 */
 	public static void main(String[] args) {				
-		String path = "C:\\Projetos\\Java\\Eclipse\\SI400_P2-ConversorUnidades\\src\\converters";
-		System.out.println(ClassFinder.LoadClasses(path).toString());
+		//String path = "C:\\Projetos\\Java\\Eclipse\\SI400_P2-ConversorUnidades\\src\\converters";
+		//System.out.println(ClassFinder.LoadClasses(path).toString());
+		double d = 10000;
+		double d2 = 0.000001;
+		System.out.println(String.format("%.3E", d));
+		System.out.println(String.format("%.3E", d2));
 	}
 }
