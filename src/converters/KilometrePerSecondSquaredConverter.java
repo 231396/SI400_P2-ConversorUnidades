3b3accfc6f1caece7680d908cefde83c2fdf53d9
LucasSalu <samuel_6000@outlook.com>
2020-11-22 20:00:52 -0300

    classe kilometrePerScondf arrumada

---
+++ src/converters/KilometrePerSecondSquaredConverter.java
@@ -0,0 +1,32 @@
+package converters;
+
+public class KilometrePerSecondSquaredConverter extends AbstractConverter {
+	
+	@Override
+	protected MeasureType setMeasureType() {
+		return MeasureType.ACCELERATION;
+	}
+	
+	@Override
+	public double toBasicUnit(double unit) {
+		return unit / 1000;
+	}
+
+	@Override
+	public double fromBasicUnit(double unit) {
+		return unit * 1000;
+	}
+	
+	@Override
+	public String toStringBr() {
+		return "Aceleração - Quilômetro p/ segundo ao quadrado (k/s\u00b2)";
+	}
+	
+	@Override
+	public String toStringNa() {
+		return "Acceleration - metre p/ second squared (m/s\u00b2)";
+	}
+
+
+
+}
