ad4d3d19f26dd2b0fa5545fcc1b5a922f68f252d
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2020-11-23 00:25:14 -0300

    No Plagiar Irei 3

--- src/base/ClassFinder.java
+++ src/base/ClassFinder.java
@@ -13,13 +13,16 @@ import converters.AbstractConverter;
  * Class that find and instantiate AbstractConverter
  */
 public final class ClassFinder {
-
-	//private static String[] prohibitedTexts = new String[] { "AbstractConverter", "MeasureType", "ClassFinder", };
-
+	
+	/**
+	 * Array in which each value of it represents a class that is not part of the family of converters
+	 */
+	private final static String[] prohibitedTexts = new String[] { "AbstractConverter", "MeasureType" };
+	
 	private final static String convertersFolder = "converters";
 
 	public static ArrayList<AbstractConverter> getExternalWorkloads(String jarName) {
-		//jarName = "C:\\Users\\Vitor\\Desktop\\teste.jar";
+
 		String packageName = convertersFolder;
 
 		packageName = packageName.replaceAll("\\.", "/");
@@ -40,28 +43,31 @@ public final class ClassFinder {
 				
 				String jarString = jarEntry.getName();
 
+				for (String str : prohibitedTexts)
+					if (str.equals(jarString))
+						continue;
+				
 				if (jarString.startsWith(packageName) && jarString.endsWith(".class")) {
 					String clsName = jarString.replaceAll("/", "\\.");
 
 					clsName = clsName.replaceAll(".class", "");
 
-					if (clsName.contains("$") == false) {
+					//if (clsName.contains("$") == false) {
 						//ClassLoader.getSystemClassLoader();
-						URL url = new URL("jar:file:" + jarName + "!/");
+					URL url = new URL("jar:file:" + jarName + "!/");
 
-						URLClassLoader ucl = new URLClassLoader(new URL[] { url });
+					URLClassLoader ucl = new URLClassLoader(new URL[] { url });
 
-						try {
-							Class<?> myLoadedClass = ucl.loadClass(clsName);
-
-							AbstractConverter myClass = (AbstractConverter) myLoadedClass.getDeclaredConstructor().newInstance();
-							myClasses.add(myClass);
-						} 
-						catch (Exception e) {
-							e.printStackTrace();
-						}
-						ucl.close();
+					try {
+						Class<?> myLoadedClass = ucl.loadClass(clsName);
+						AbstractConverter myClass = (AbstractConverter) myLoadedClass.getDeclaredConstructor().newInstance();
+						myClasses.add(myClass);
+					} 
+					catch (Exception e) {
+						e.printStackTrace();
 					}
+					ucl.close();
+					//}
 				}
 			}
 
