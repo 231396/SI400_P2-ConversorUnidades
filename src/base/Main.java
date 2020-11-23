8463b5372fa5bf9de39c2c8be02914b86acc8d46
Vitor Prado <vvpll@outlook.com>
2020-11-23 00:55:35 -0300

    Linguagem adicionada

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import converters.AbstractConverter;
 
@@ -10,24 +11,30 @@ public class Main {
 	 * 
 	 * @param args arguments passed from console
 	 */
-	public static void main(String[] args) {				
-		
-		
-		Controller.jarFileName = new java.io.File(Main.class.getProtectionDomain()
-				  .getCodeSource()
-				  .getLocation()
-				  .getPath())
-				.getName();
-		
-		Controller.programLanguage = Languages.ENGLISH;
-		
-		
+	public static void main(String[] args) {
+
+		Locale language = Locale.getDefault();
+
+		if (args.length > 0) {
+			language = new Locale(args[0]);
+		}
+
+		Controller.jarFileName = new java.io.File(
+				Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
+
+
+		if (Languages.PORTUGUESE.getLanguage().equals(language.getLanguage())) {
+			Controller.programLanguage = Languages.PORTUGUESE;
+		}else {
+			Controller.programLanguage = Languages.ENGLISH;
+		}
+
 		ArrayList<AbstractConverter> list = Controller.getAllConverters();
-		
+
 		System.out.println(list.size());
 		for (AbstractConverter a : list) {
 			System.out.println(a);
 		}
-		
+
 	}
 }
